f875c3e6d46fe924623b66abb7bc7b888270a089
Dopzhik <haltura24@gmail.com>
2023-07-02 19:43:08 +0800

    задача 2 выполнена

---
+++ task2/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Задача 2: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+int ReadInt(string message)
+{
+    System.Console.Write(message);              
+    return Convert.ToInt32(Console.ReadLine()); 
+}
+int SumDigits(int number)
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        sum = sum + number % 10;
+        number /= 10;
+    }
+    return sum;
+}
+int number = ReadInt("Введите число => ");
+System.Console.WriteLine($"Сумма цифр в числе {number} равна {SumDigits(number)}");
